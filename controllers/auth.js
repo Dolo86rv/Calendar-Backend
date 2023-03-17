cefd6b5a5408bad751b9355423e8d884934c586a
Dolorv86 <dolorv86@gmail.com>
2023-03-17 10:17:52 +0100

    Se crea el backend de la app calendar

---
+++ controllers/auth.js
@@ -0,0 +1,107 @@
+const { response } = require('express')
+const Usuario = require('../models/Usuario')
+const bcrypt = require('bcryptjs')
+const { generarJWT } = require('../helpers/jwt')
+
+const crearUsuario = async(req, res = response )=>{
+    
+    const { email, password } = req.body
+
+    try {
+
+        let usuario = await Usuario.findOne({ email})
+        if( usuario ){
+            return res.status(400).json({
+                ok:false,
+                msg:"Existe un usauario con ese correo electronico"
+            })
+        }
+        
+        usuario = new Usuario( req.body )
+
+        //Encriptar contraseña
+        const salt= bcrypt.genSaltSync()
+        usuario.password = bcrypt.hashSync( password, salt)
+
+        await usuario.save()
+        
+        //Generar JWT
+        const token = await generarJWT( usuario.id, usuario.name)
+
+        return res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name:usuario.name,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg:'Por favor consulte al administrador'
+        })
+        
+    }
+}
+
+const loginUsuario = async(req, res = response)=>{
+    
+    const { email, password } = req.body
+
+    try {
+        
+        const usuario = await Usuario.findOne({ email})
+        if( !usuario ){
+            return res.status(400).json({
+                ok:false,
+                msg:"El usuario y contraseña no son correctos"
+            })
+        }
+
+        //confirmar los passwords
+        const validPassword = bcrypt.compareSync( password, usuario.password)
+
+        if( !validPassword ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password Incorrecto'
+            })
+        }
+        //Generar JWT
+        const token = await generarJWT( usuario.id, usuario.name)
+
+        res.json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg:'Por favor consulte al administrador'
+        })
+    }
+}
+
+const revalidarToken = async(req, res = response)=>{
+    
+    const { uid, name } = req
+
+    const token = await generarJWT( uid, name )
+
+    res.json({
+        ok: true,
+        token
+    })
+}
+
+
+module.exports = { 
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
